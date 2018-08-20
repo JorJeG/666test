f063ab9145a77cd999404fd94c93342f10ff7e0f
Georgiy Starkov <gstarkov658@gmail.com>
2018-08-20 22:58:17 +0300

    Добавит сборку стилей

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp'),
       pug = require('gulp-pug'),
-      del = require('del');
+      del = require('del'),
+      sass = require('gulp-sass'),
+      prefixer = require('gulp-autoprefixer'),
+      sourcemap = require('gulp-sourcemaps');
 
 function buildHTML() {
   return gulp.src('./src/templates/*.pug')
@@ -8,10 +11,20 @@ function buildHTML() {
       pretty: true
     }))
     .pipe(gulp.dest('./build'));
-};
+}
 
 function cleanHTML() {
   return del(['build/index.html']);
 }
 
-gulp.task('pug', gulp.series(cleanHTML, buildHTML));
+function buildCSS() {
+  return gulp.src('./src/sass/*.scss')
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(sourcemap.write())
+    .pipe(prefixer())
+    .pipe(gulp.dest('./build'))
+}
+
+gulp.task('html', gulp.series(cleanHTML, buildHTML));
+gulp.task('css', buildCSS);
