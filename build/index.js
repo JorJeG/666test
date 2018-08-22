18ad107cffb03770bbb4c8477ca8a83de03d2663
Georgiy Starkov <gstarkov658@gmail.com>
2018-08-22 09:23:16 +0300

    Добавит обработку нажатия на звёздочку

--- build/index.js
+++ build/index.js
@@ -1,9 +1,14 @@
-const more = document.getElementById('more');
-const rooms = document.querySelector('.rooms');
-const tab = document.querySelector('.nav__tab');
-const aside = document.querySelector('.page__aside');
+const more = document.getElementById('more'),
+      rooms = document.querySelector('.rooms'),
+      tab = document.querySelector('.nav__tab'),
+      aside = document.querySelector('.page__aside'),
+      main = document.querySelector('.main');
 
 more.addEventListener('click', (e) => {
+  /**
+   * TODO: Здесь должен быть запрос на сервер с указанием 
+   * сколько квартир пропустить с начала, пагинация
+   */
   e.preventDefault();
   fetch('/data')
     .then(resp => resp.text())
@@ -17,3 +22,14 @@ tab.addEventListener('click', (e) => {
   aside.classList.toggle('page__aside_open');
   tab.classList.toggle('nav__tab_open');
 });
+
+main.addEventListener('click', (e) => {
+  /**
+   * TODO: Здесь должен быть пост запрос на сервер для
+   * изменения состояния карточки
+   */
+  const target = e.target.classList.contains('card__favorite');
+  if (target) {
+    e.target.classList.toggle('card__favorite_selected');
+  }
+}, false);
